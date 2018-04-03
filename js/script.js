8e3ddde2dcbe6976f9fd14c01fa820388c54ed2f
Lisa Reinhardt <reinal@gmail.com>
2018-04-03 17:15:26 -0500

    Added a dummy request to REST API.

--- js/script.js
+++ js/script.js
@@ -6,4 +6,22 @@ $(function () {
 		"width": "100%",
 		"tags": true
 	});
+	// Eat In
+	$("#eatIn").click(function (e) {
+		// Prevent the default event of submitting the form.
+		e.preventDefault();
+		// Send a GET Request and fetch JSON.
+		$.ajax({
+			// Generate the URL from the input received.
+			"url": "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ranking=1&number=5&ingredients=apples%2Cflour%2Csugar",
+			"headers": {
+				"X-Mashape-Key": "APHt8X9YagmshN6vXr6VkafMcAy1p1sIjtzjsndjj2LopWsrpl",
+				"X-Mashape-Host": "spoonacular-recipe-food-nutrition-v1.p.mashape.com"
+			},
+			"success": function (res) {
+				console.log(res);
+			},
+			"crossDomain": true
+		});
+	});
 });
