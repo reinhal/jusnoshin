d7699ef0d386518a65e6457870b267708159b6cb
Lisa Reinhardt <reinal@gmail.com>
2018-04-09 12:23:14 -0500

    -- INSERT -- Updated classed to have 'hidden' property. Added a function to display the first  results of recipes.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function () {
-	$("#diet-req, #diet-cui" ).select2({
+	$("#diet-req, #diet-cui").select2({
 		"width": "100%"
 	});
 	$(".lisa-select2-tags").select2({
@@ -24,7 +24,76 @@ $(function () {
 			},
 			"method": "GET",
 			"success": function (res) {
-				console.log(res);
+data = {
+  "results": [
+  {
+			"id": 487522,
+			"usedIngredientCount": 1,
+			"missedIngredientCount": 30,
+			"likes": 0,
+			"title": "veg biryani , how to make hyderabadi veg biryani",
+			"image": "https://spoonacular.com/recipeImages/487522-312x231.jpg",
+			"imageType": "jpg"
+		}, {
+			"id": 263868,
+			"usedIngredientCount": 1,
+			"missedIngredientCount": 10,
+			"likes": 0,
+			"title": "Red Lentil Curry Soup",
+			"image": "https://spoonacular.com/recipeImages/263868-312x231.jpg",
+			"imageType": "jpg"
+		}, {
+			"id": 289532,
+			"usedIngredientCount": 1,
+			"missedIngredientCount": 14,
+			"likes": 0,
+			"title": "Saag Paneer: Spinach with Indian Cheese",
+			"image": "https://spoonacular.com/recipeImages/289532-312x231.jpeg",
+			"imageType": "jpeg"
+		}, {
+			"id": 516612,
+			"usedIngredientCount": 1,
+			"missedIngredientCount": 7,
+			"likes": 0,
+			"title": "Indian Spiced Oatmeal (or a healthy version of Kheer)",
+			"image": "https://spoonacular.com/recipeImages/516612-312x231.jpg",
+			"imageType": "jpg"
+	}, {
+			"id": 764910,
+			"usedIngredientCount": 1,
+			"missedIngredientCount": 11,
+			"likes": 0,
+			"title": "Red Curry Lemongrass Soup",
+			"image": "https://spoonacular.com/recipeImages/764910-312x231.jpg",
+			"imageType": "jpg"
+		}, {
+			"id": 217193,
+			"usedIngredientCount": 1,
+			"missedIngredientCount": 13,
+			"likes": 0,
+			"title": "Goat curry",
+			"image": "https://spoonacular.com/recipeImages/217193-312x231.jpg",
+			"imageType": "jpg"
+	  }
+  ],
+	"baseUri": "https://spoonacular.com/recipeImages/",
+  "offset": 0,
+	"number": 6,
+  "totalResults": 103,
+	"processingTimeMs": 188
+}
+				$("#num").text(res.totalResults);
+				$("#term").text("You searched for " + dietReq + " options under " + cui + " that includes " + desIng + " and not " + intAll);
+				$("#resultsPerPage").text(res.number);
+				$(".results").append(function () {
+					var finalHTML = "";
+					var ress = res.results;
+					for (var i = 0; i < ress.length; i++)
+						finalHTML += '<li><figure><img src="' + ress[i].image + '" alt="' + ress[i].title + '" /><figcaption><a href="">' + ress[i].title + '</a></figcaption></figure></li>';
+					return finalHTML;
+				});
+				$("#intro").addClass("no-intro");
+				$("#resRec").prop("hidden", false);
 			},
 			"crossDomain": true
 		});
