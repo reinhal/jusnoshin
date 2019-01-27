3aefd8300297904194bdf36ab9c152c8e846e984
Lisa Reinhardt <reinhal@gmail.com>
2019-01-27 12:54:44 -0600

    refactored code to meet better coding standards

--- js/script.js
+++ js/script.js
@@ -1,69 +1,15 @@
-	// $("#diet-req, #diet-cui, #excl-ing").select2({ // create accessible friendly input fields with bootstrap
-	// 	"width": "100%"
-	// });
-	// $(".lisa-select2-tags").select2({
-	// 	"width": "100%",
-	// 	"tags": true
-	// });
-	// Get meal ideas
-	// var offset = 0;
-	// $("#eatIn, #eatInAgain, #moreIdeas").click(function (e) {
-	// 	// Prevent the default event of submitting the form.
-	// 	e.preventDefault();
-	// 	if ( this.id == 'moreIdeas') 
-	// 		offset += 6;
-	// 	// Send a GET Request and fetch JSON.
-	// 	var dietReq = $('#diet-req').val();
-	// 	var cui = $('#diet-cui').val();
-	// 	var intAll = $('#excl-ing').val();
-	// 	$.ajax({
-	// 		// Generate the URL from the input received.
-	// 		"url": "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?number=6&offset=" + offset + "&diet=" + encodeURIComponent(dietReq) + "&cuisine=" + encodeURIComponent(cui) + "&intolerances=" + encodeURIComponent(intAll),
-	// 		"headers": {
-	// 			"X-Mashape-Key": "APHt8X9YagmshN6vXr6VkafMcAy1p1sIjtzjsndjj2LopWsrpl",
-	// 			"X-Mashape-Host": "spoonacular-recipe-food-nutrition-v1.p.mashape.com"
-	// 		},
-	// 		"method": "GET",
-	// 		"success": function (res) {
-	// 			$(".results").empty().append(function () { // display data retrieved from URL.  Six recipes. 
-	// 				var optionsHTML = "";
-	// 				var ress = res.results;
-	// 				for (var i = 0; i < ress.length; i++)
-	// 					optionsHTML += '<li data-id="' + ress[i].id + '"><figure><img src="' + ress[i].image + '" alt="' + ress[i].title + '" /><figcaption><a target="_blank" href="">' + ress[i].title + '</a></figcaption></figure></li>';
-	// 				return optionsHTML;
-	// 			});
-				// This could be one functin to hide stuff
 
 var offset = 0;
-
-function displayWinePairing() {
-	var cui = $('#diet-cui').val();
-	$.ajax({
-		"url": "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/wine/pairing?food=" + cui,
-		"headers": {
-			"X-Mashape-Key": "APHt8X9YagmshN6vXr6VkafMcAy1p1sIjtzjsndjj2LopWsrpl",
-			"X-Mashape-Host": "spoonacular-recipe-food-nutrition-v1.p.mashape.com"
-		},
-		"method": "GET",
-		"success": function (wine) {
-			$(".wine").append(function () {  //display the paired wine based on cuisine selected.  A default wine is offered if no pairing is available. 
-				console.log(wine);
-				if (wine.productMatches.length == 0) {
-					var defaultWineHTML = '<figure><img target="_blank" src="https://spoonacular.com/productImages/442371-312x231.jpg" alt="Chehalem INOX Chardonnay" /><figcaption><a href="https://click.linksynergy.com/deeplink?id=*QCiIS6t4gA&mid=2025&murl=https%3A%2F%2Fwww.wine.com%2Fproduct%2Fchehalem-inox-chardonnay-2009%2F109305">\'"INOX® takes its name from the abbreviation of the French word for stainless steel, inoxydable. The wine was created differently from most Chardonnay you\'ve had. We think we\'ve succeeded in expressing the crisp, steely, and fruit-rich side that we love about some Old-World Chardonnays. What makes this possible is the use of exclusively Dijon clones, exceptionally well suited to Oregon\'s cool climate and exhibiting a richness that does not depend on oak. INOX screams of the hallmarks of a cool climate-brightness, pinpoint fruit, and explosive aromas and flavors. We intend INOX for a full range of use, from hot weather chilling to elegant dinner complements.Quintessential INOX, with lovely white aromas of gardenia and other flowers, peach, apricot, pear, pineapple, and green apple candy showing on the nose and palate; it shows a great balance with relatively low alcohol for the year and bright acid; the length is lovely and the weight rich, with a supple, silky texture; flavors linger, with cherry and peach accents. Very pleased."\'</a></figcaption></figure>';
-					return defaultWineHTML;
-				} else {
-				var wineHTML = '<figure><img src="' + wine.productMatches[0].imageUrl + '" alt="' + wine.productMatches[0].title + '" /><figcaption><a href="' + wine.productMatches[0].link + '">' + wine.productMatches[0].description + '</a></figcaption></figure>';
-				return wineHTML;
-				}
-			});
-		}
-	});
-	$("#startOver").click(function () {  // allows user to start the search over
-	location.reload();
-	});
+let pageLimit = 6;
+let apiBaseUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com';
+let apiRecipeBaseUrl =  apiBaseUrl + '/recipes'; 
+let apiWineBaseUrl = apiBaseUrl + '/food/wine';
+let apiHeaderAuthorization = {
+	"X-Mashape-Key": "APHt8X9YagmshN6vXr6VkafMcAy1p1sIjtzjsndjj2LopWsrpl",
+	"X-Mashape-Host": "spoonacular-recipe-food-nutrition-v1.p.mashape.com"
 }
 
-function inputFields() {
+function displayInputDropdowns() {
 	$("#diet-req, #diet-cui, #excl-ing").select2({ // create accessible friendly input fields with bootstrap
 		"width": "100%"
 	});
@@ -73,62 +19,111 @@ function inputFields() {
 	});
 }
 
-function getMealIdeas() {
+function getHtmlForRecipe(recipe) {
+	return '<li data-id="' + recipe.id + '">'
+	+ '<figure>'
+	   + '<img src="' + recipe.image + '" alt="' + recipe.title + '" />'
+	   + '<figcaption>'
+		   + '<a target="_blank" href="">' + recipe.title + '</a>'
+	   + '</figcaption>' 
+   + '</figure>' 
++ '</li>';
+}
+function handleGetMealIdeasSubmit() {
 	$("#eatIn, #eatInAgain, #moreIdeas").click(function (e) {
 		e.preventDefault();
 		if ( this.id == 'moreIdeas') 
-			offset += 6;
-		var dietReq = $('#diet-req').val();
-		var cui = $('#diet-cui').val();
-		var intAll = $('#excl-ing').val();
+			offset += pageLimit;
+
+		var dietRequirements = $('#diet-req').val();
+		var typeOfCuisine = $('#diet-cui').val();
+		var intolerancesAndAllergies = $('#excl-ing').val();
 		$.ajax({
 			// Generate the URL from the input received.
-			"url": "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?number=6&offset=" + offset + "&diet=" + encodeURIComponent(dietReq) + "&cuisine=" + encodeURIComponent(cui) + "&intolerances=" + encodeURIComponent(intAll),
-			"headers": {
-				"X-Mashape-Key": "APHt8X9YagmshN6vXr6VkafMcAy1p1sIjtzjsndjj2LopWsrpl",
-				"X-Mashape-Host": "spoonacular-recipe-food-nutrition-v1.p.mashape.com"
-			},
+			"url": apiRecipeBaseUrl 
+				+ "/searchComplex"
+				+ "?number=6"
+				+ "&offset=" + offset
+				+ "&diet=" + encodeURIComponent(dietRequirements)
+				+ "&cuisine=" + encodeURIComponent(typeOfCuisine) 
+				+ "&intolerances=" + encodeURIComponent(intolerancesAndAllergies),
+			'headers': apiHeaderAuthorization,
 			"method": "GET",
 			"success": function (res) {
 				$(".results").empty().append(function () { // display data retrieved from URL.  Six recipes. 
-					var optionsHTML = "";
-					var ress = res.results;
-					for (var i = 0; i < ress.length; i++)
-						optionsHTML += '<li data-id="' + ress[i].id + '"><figure><img src="' + ress[i].image + '" alt="' + ress[i].title + '" /><figcaption><a target="_blank" href="">' + ress[i].title + '</a></figcaption></figure></li>';
-					return optionsHTML;
+					var recipes = res.results;
+					return recipes.map(value => {
+						return getHtmlForRecipe(value)
+					});
 				});
 				hideButtons();
+				handleDrilldownIntoSingleRecipe();
 			}
 		});
 	});
 }
 
-function getFullRecipe() {
-
-}
-
-function displaySingleRecipe() {
+function handleDrilldownIntoSingleRecipe() {
 	$(".results li").click(function (e) {
 		e.preventDefault();
-		var recId = $(this).data("id");  //id.results  this is the variable for the id of the recipe the user clicks
+		var recipeId = $(this).data("id");  //id.results  this is the variable for the id of the recipe the user clicks
 		$.ajax({
-			"url": "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" + recId + "/information",
-			"headers": {
-				"X-Mashape-Key": "APHt8X9YagmshN6vXr6VkafMcAy1p1sIjtzjsndjj2LopWsrpl",
-				"X-Mashape-Host": "spoonacular-recipe-food-nutrition-v1.p.mashape.com"
-			},
+			"url": apiRecipeBaseUrl + '/' + recipeId + "/information",
+			'headers': apiHeaderAuthorization,
 			"method": "GET",
 			"success": function (rs) {
 				$(".recipe").append(function () {   // display the selected recipe which is linked to the complete recipe. 
-					var reci = rs;
+					var recipe = rs;
 					console.log(rs);
-					var recipeHTML = '<figure><img src="' + reci.image + '" alt="' + reci.title + '" /><figcaption><a target="_blank" href="' + reci.sourceUrl + '">' + reci.title + '</a></figcaption></figure>';
+					var recipeHTML = getHtmlForRecipe(recipe)
 					return recipeHTML;
 				});
 				$("#resRec").prop("hidden", true);
 				$("#recView").prop("hidden", false);
+				displayWinePairing();
 			}
 		});
+	});
+}
+
+function displayWinePairing() {
+	var cui = $('#diet-cui').val();
+	$.ajax({
+		"url": apiWineBaseUrl + "/pairing?food=" + cui,
+		"headers": apiHeaderAuthorization,
+		"method": "GET",
+		"success": function (wine) {
+			$(".wine").append(function () {  //display the paired wine based on cuisine selected.  A default wine is offered if no pairing is available. 
+				if (wine.productMatches.length == 0) {
+					var defaultWineHTML = 
+						'<figure>'
+							+ '<img target="_blank" src="https://spoonacular.com/productImages/442371-312x231.jpg" alt="Chehalem INOX Chardonnay" />'
+							+ '<figcaption>'
+								+'<a href="https://click.linksynergy.com/deeplink?id=*QCiIS6t4gA&mid=2025&murl=https%3A%2F%2Fwww.wine.com%2Fproduct%2Fchehalem-inox-chardonnay-2009%2F109305">'
+								+ '\'"INOX® takes its name from the abbreviation of the French word for stainless steel, inoxydable. The wine was created differently from most Chardonnay you\'ve' 
+								+ ' had. We think we\'ve succeeded in expressing the crisp, steely, and fruit-rich side that we love about some Old-World Chardonnays. What makes this possible is'
+								+ ' the use of exclusively Dijon clones, exceptionally well suited to Oregon\'s cool climate and exhibiting a richness that does not depend on oak. INOX screams' 
+								+ ' of the hallmarks of a cool climate-brightness, pinpoint fruit, and explosive aromas and flavors. We intend INOX for a full range of use, from hot weather'
+								+ ' chilling to elegant dinner complements.Quintessential INOX, with lovely white aromas of gardenia and other flowers, peach, apricot, pear, pineapple,'
+								+ ' and green apple candy showing on the nose and palate; it shows a great balance with relatively low alcohol for the year and bright acid; the length' 
+								+ ' is lovely and the weight rich, with a supple, silky texture; flavors linger, with cherry and peach accents. Very pleased."\'</a>'
+							+ '</figcaption>'
+						+ '</figure>';
+					return defaultWineHTML;
+				}
+				let getWineHTML =
+				'<figure>'
+					+ '<img src="' + wine.productMatches[0].imageUrl + '" alt="'+ wine.productMatches[0].title + '" />'
+					+ '<figcaption>'
+						+ '<a target="_blank" href="' + wine.productMatches[0].link + '">' + wine.productMatches[0].description + '</a>'
+					+ '</figcaption>'
+				+ '</figure>';
+				return getWineHTML;	
+			});
+		}
+	});
+	$("#startOver").click(function () {  // allows user to start the search over
+	location.reload();
 	});
 }
 
@@ -139,9 +134,7 @@ function hideButtons() {
 }
 
 $(function () {
-	inputFields();
-	getMealIdeas();
-	hideButtons();
-	displaySingleRecipe();
-	displayWinePairing();
+	displayInputDropdowns();
+	handleGetMealIdeasSubmit();
+	handleDrilldownIntoSingleRecipe();
 });
